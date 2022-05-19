549b4e064dff75de1cd1737507ad40e4d30ccbae
Jade <67784865+jadedye@users.noreply.github.com>
2022-05-19 18:16:47 +0100

    update 2

---
+++ database.js
@@ -0,0 +1,15 @@
+ /**
+   * Class: level: spell level
+   * wizard.2.4
+   **/
+  const database = {
+    "wizard": {
+      "classLevel1": {
+        "spellLevel1": 3, 
+        "spellLevel2": 4, 
+        "spellLevel3": 5},
+      "classLevel2": 3,
+      3: 4,
+      4: 5
+    }
+  }
---
+++ helper.js
@@ -0,0 +1,21 @@
+/**
+ * Gets the class from the class dropdown
+ */
+function getClass(){
+    let chosenClass = document.getElementId("classChoice").value;
+    console.log("your class is...", chosenClass)
+}
+
+/**
+ * Gets the level 
+ */
+function getLevel(){
+
+}
+
+/**
+ * Populates spell slots table
+ */
+function displaySpellSlots(){
+
+}
